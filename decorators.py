b4302d24380f784b061082c6a9e29a02e998b444
Greg <34799273+GrzegorzMika@users.noreply.github.com>
2020-05-28 18:06:06 +0200

    Update decorators.py

--- decorators.py
+++ decorators.py
@@ -83,3 +83,12 @@ def log(msg, logger=None):
         logger.warning(msg)
     else:
         print(msg)
+        
+
+def exception_handler(func):
+    def inner_function(*args, **kwargs):
+        try:
+            func(*args, **kwargs)
+        except TypeError:
+            print(f"{func.__name__} only takes numbers as the argument")
+    return inner_function
